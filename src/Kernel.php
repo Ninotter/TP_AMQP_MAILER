7652ca9fc3a87d7d8ae979ab248bfe9ca9e71170
Ninotter <nino.didier@live.fr>
2023-11-24 12:30:12 +0100

    mailer, integration

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Processes\EmailProcess;
+use App\Services\AmqpListenerService;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
@@ -9,3 +11,10 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
+
+$amqpListenerService = new AmqpListenerService();
+$amqpListenerService->init();
+// $emailProcess = new EmailProcess();
+// $amqpListenerService->setCallback(function ($msg) use ($emailProcess) {
+//     $emailProcess->SendConfirmEmail($msg->body);
+// });
